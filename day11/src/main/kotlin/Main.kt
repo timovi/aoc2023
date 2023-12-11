393439019c03f251b4043a2f9fcfd52fa18050f8
Timo Viisteensaari <timo.viisteensaari@solita.fi>
2023-12-11 19:24:38 +0200

    Day11 puzzle2

--- day11/src/main/kotlin/Main.kt
+++ day11/src/main/kotlin/Main.kt
@@ -1,77 +1,86 @@
 import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 import kotlin.system.measureTimeMillis
 
 data class Coordinate(val x: Int, val y: Int)
-data class MapAndGalaxies(val map: List<List<Char>>, val galaxies: List<Coordinate>)
+data class Universe(val emptyRows: Set<Int>, val emptyCols: Set<Int>, val galaxies: List<Coordinate>)
 
 fun main() {
     val duration = measureTimeMillis {
         val input = {}::class.java.getResource("input.txt").readText()
-        val universe = expand(input.split("\r\n").map { it.toList() })
+        val universe = universe(input.split("\r\n").map { it.toList() })
 
-        val pathLengths = allDistances(universe.galaxies)
-        println("Puzzle 1: ${pathLengths.sum()}") // 9591768
-        println("Puzzle 2: ")
+        println("Puzzle 1: ${distances(2, universe).sum()}")
+        println("Puzzle 2: ${distances(1000000, universe).sum()}")
     }
 
     println("Duration: $duration ms")
 }
 
-fun expand(map: List<List<Char>>): MapAndGalaxies {
-    val newMap = mutableListOf<List<Char>>()
+fun universe(map: List<List<Char>>): Universe {
+    val emptyRows = mutableSetOf<Int>()
+    val emptyCols = mutableSetOf<Int>()
     val galaxyCoordinates = mutableListOf<Coordinate>()
 
     for (y in map.indices) {
-        newMap.add(map[y])
         if (!map[y].contains('#')) {
-            newMap.add(map[y])
+            emptyRows.add(y)
         }
     }
 
-    val columnsWithoutGalaxies = mutableListOf<Int>()
-    for (x in newMap[0].indices) {
+    for (x in map[0].indices) {
         var containsGalaxy = false
-        for (y in newMap.indices) {
-            if (newMap[y][x] == '#') {
+        for (y in map.indices) {
+            if (map[y][x] == '#') {
+                galaxyCoordinates.add(Coordinate(x, y))
                 containsGalaxy = true
-                break
             }
         }
         if (!containsGalaxy) {
-            columnsWithoutGalaxies.add(x)
+            emptyCols.add(x)
         }
     }
 
-    for (y in newMap.indices) {
-        var newRow = ""
-        for (x in newMap[y].indices) {
-            newRow += newMap[y][x]
-            if (columnsWithoutGalaxies.contains(x)) {
-                newRow += '.'
-            }
-        }
-        newMap[y] = newRow.toList()
-    }
-
-    for (y in newMap.indices) {
-        for (x in newMap[y].indices) {
-            if (newMap[y][x] == '#') {
-                galaxyCoordinates.add(Coordinate(x, y))
-            }
-        }
-    }
-
-    return MapAndGalaxies(newMap, galaxyCoordinates)
+    return Universe(emptyRows, emptyCols, galaxyCoordinates)
 }
 
-fun allDistances(galaxies: List<Coordinate>): List<Int> {
-    val distances = mutableListOf<Int>()
-    for (i in galaxies.indices) {
-        for (j in i + 1..<galaxies.size) {
-            distances += manhattanDistance(galaxies[i], galaxies[j])
+fun distances(expansionFactor: Int, universe: Universe): List<Long> {
+    val distances = mutableListOf<Long>()
+    for (i in universe.galaxies.indices) {
+        for (j in i + 1..<universe.galaxies.size) {
+            distances += expansedDistance(
+                universe.galaxies[i],
+                universe.galaxies[j],
+                universe.emptyRows,
+                universe.emptyCols,
+                expansionFactor
+            )
         }
     }
     return distances
+}
+
+fun expansedDistance(
+    from: Coordinate,
+    to: Coordinate,
+    emptyRows: Set<Int>,
+    emptyCols: Set<Int>,
+    expansionFactor: Int
+): Long {
+    val minX = min(from.x, to.x)
+    val minY = min(from.y, to.y)
+    val maxX = max(from.x, to.x)
+    val maxY = max(from.y, to.y)
+    val emptyRowsBetween = emptyRows.count { it in minY..maxY }
+    val emptyColsBetween = emptyCols.count { it in minX..maxX }
+
+    val directDistance = manhattanDistance(from, to)
+
+    val rowsToAdd = if (emptyRowsBetween > 0) (emptyRowsBetween * (expansionFactor - 1)) else 0
+    val colsToAdd = if (emptyColsBetween > 0) (emptyColsBetween * (expansionFactor - 1)) else 0
+
+    return directDistance.toLong() + rowsToAdd + colsToAdd
 }
 
 fun manhattanDistance(from: Coordinate, to: Coordinate) =
