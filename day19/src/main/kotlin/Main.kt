6a10bee70e59dc994a558af6613fa733617c03bb
Timo Viisteensaari <timo.viisteensaari@solita.fi>
2023-12-20 11:30:42 +0200

    Day19 puzzle2

--- day19/src/main/kotlin/Main.kt
+++ day19/src/main/kotlin/Main.kt
@@ -14,16 +14,24 @@ enum class Comparator {
     }
 }
 
-data class Rule(
-    val partCategory: Char?,
-    val comparator: Comparator?,
-    val comparatorValue: Int?,
-    val ifTrue: String
-)
-
 data class Part(
     val categories: Map<Char, Int>
 )
+
+data class Condition(
+    val partCategory: Char,
+    val comparator: Comparator,
+    val comparatorValue: Int,
+)
+
+data class Node(
+    val condition: Condition? = null,
+    val ifTrue: Node? = null,
+    val ifFalse: Node? = null,
+    val isAccepted: Boolean? = null
+)
+
+val paths = mutableListOf<List<Node>>()
 
 fun main() {
     val duration = measureTimeMillis {
@@ -34,77 +42,151 @@ fun main() {
         val parts = parseParts(rows)
 
         // Puzzle 1
-        //workflows.forEach(::println)
-        //parts.forEach(::println)
-
-        val accepted = parts.filter { isAccepted(it, workflows, workflows["in"]!!) }
+        val accepted = parts.filter { isAccepted(it, workflows) }
         println("Puzzle 1: ${accepted.sumOf { it.categories.values.sum() }}")
 
         // Puzzle 2
-        println("Puzzle 2: ")
+        populateAcceptedPaths(workflows)
+        println("Puzzle 2: ${rangeCombinations(paths)}")
     }
     println("Duration: $duration ms")
 }
 
-fun isAccepted(part: Part, workflows: Map<String, List<Rule>>, currentWorkflow: List<Rule>): Boolean {
-    for (rule in currentWorkflow) {
-        if (rule.comparator == null && rule.accepts()) {
-            return true
-        }
-        if (rule.comparator == null && rule.rejects()) {
-            return false
-        }
-        if (rule.comparator == null) {
-            val nextWorkflow = workflows[rule.ifTrue]!!
-            return isAccepted(part, workflows, nextWorkflow)
-        }
+fun isAccepted(part: Part, node: Node): Boolean {
+    if (node.isAccepted != null) {
+        return node.isAccepted
+    }
 
-        val partValue = part.categories[rule.partCategory]!!
-        val comparisonResult = rule.compareTo(partValue)
-
-        if (comparisonResult && rule.accepts()) {
-            return true
-        }
-        if (comparisonResult && rule.rejects()) {
-            return false
-        }
-        if (comparisonResult) {
-            val nextWorkflow = workflows[rule.ifTrue]!!
-            return isAccepted(part, workflows, nextWorkflow)
+    if (node.condition != null) {
+        val partValue = part.categories[node.condition.partCategory]!!
+        val comparisonResult = node.condition.run(partValue)
+        return if (comparisonResult) {
+            isAccepted(part, node.ifTrue!!)
+        } else {
+            isAccepted(part, node.ifFalse!!)
         }
     }
-    return false
+
+    return isAccepted(part, node.ifTrue!!)
 }
 
-fun parseWorkflows(rows: List<String>) =
-    rows.takeWhile { it.isNotBlank() }
-        .map(::parseWorkflow)
-        .associate { (name, rules) -> name to rules }
+fun Condition.run(partValue: Int): Boolean {
+    return when (this.comparator) {
+        Comparator.LESS_THAN -> partValue < this.comparatorValue
+        Comparator.GREATER_THAN -> partValue > this.comparatorValue
+    }
+}
+
+fun parseWorkflows(rows: List<String>): Node {
+    val workflowRows = rows.takeWhile { it.isNotBlank() }.associate(::parseWorkflow)
+    val start = workflowRows["in"]!!
+    return parseNode(start.first(), start.drop(1), workflowRows)
+}
+
+fun parseWorkflow(workflow: String): Pair<String, List<String>> {
+    val (name, rest) = workflow.split("{")
+    val rules = rest.dropLast(1).split(",")
+    return name to rules
+}
+
+fun parseNode(rule: String, restRules: List<String>, workflows: Map<String, List<String>>): Node {
+    if (rule == "A") {
+        return Node(null, null, null, true)
+    }
+
+    if (rule == "R") {
+        return Node(null, null, null, false)
+    }
+
+    if (!rule.contains(":")) {
+        val nextWorkflow = workflows[rule]!!
+        val nextNode = parseNode(nextWorkflow.first(), nextWorkflow.drop(1), workflows)
+        return Node(null, nextNode, null, null)
+    }
+
+    val condition = Condition(rule[0], Comparator.parse(rule[1]), rule.substring(2, rule.indexOf(":")).toInt())
+    val next = rule.substring(rule.indexOf(":") + 1)
+
+    val nextNodeIfTrue = if (next == "A" || next == "R") {
+        parseNode(next, restRules, workflows)
+    } else {
+        val nextWorkflow = workflows[next]!!
+        parseNode(nextWorkflow.first(), nextWorkflow.drop(1), workflows)
+    }
+    val nextNodeIfFalse = parseNode(restRules.first(), restRules.drop(1), workflows)
+
+    return Node(condition, nextNodeIfTrue, nextNodeIfFalse, null)
+}
+
+fun populateAcceptedPaths(node: Node, previousNodes: List<Node> = emptyList()) {
+    if (node.ifTrue != null) {
+        populateAcceptedPaths(node.ifTrue, previousNodes + node)
+    }
+
+    if (node.ifFalse != null) {
+        val newComparatorAndValue = when (node.condition!!.comparator) {
+            Comparator.LESS_THAN -> Comparator.GREATER_THAN to node.condition.comparatorValue - 1
+            Comparator.GREATER_THAN -> Comparator.LESS_THAN to node.condition.comparatorValue + 1
+        }
+        val newNode = Node(
+            Condition(node.condition.partCategory, newComparatorAndValue.first, newComparatorAndValue.second),
+            node.ifTrue,
+            node.ifFalse,
+            node.isAccepted
+        )
+        populateAcceptedPaths(node.ifFalse, previousNodes + newNode)
+    }
+
+    if (node.isAccepted != null && node.isAccepted) {
+        paths.add(previousNodes + node)
+    }
+}
+
+fun rangeCombinations(acceptedPaths: List<List<Node>>): Long {
+    val acceptedRangesForPaths = acceptedPaths.map(::acceptedRanges).filter { it.isNotEmpty() }
+    return acceptedRangesForPaths.sumOf {
+        it['x']!!.count().toLong() * it['m']!!.count() * it['a']!!.count() * it['s']!!.count()
+    }
+}
+
+fun acceptedRanges(acceptedPath: List<Node>): Map<Char, IntRange> {
+    val ranges = mutableMapOf(
+        'x' to 1..4000,
+        'm' to 1..4000,
+        'a' to 1..4000,
+        's' to 1..4000
+    )
+
+    var containsInvalidRange = false
+    acceptedPath.dropLast(1).forEach { node ->
+        if (node.condition != null) {
+            val partCategory = node.condition.partCategory
+            val comparatorValue = node.condition.comparatorValue
+            val range = ranges[partCategory]!!
+            val newRange = when (node.condition.comparator) {
+                Comparator.LESS_THAN -> range.first..<comparatorValue
+                Comparator.GREATER_THAN -> (comparatorValue + 1)..range.last
+            }
+
+            if (newRange.first > newRange.last) {
+                containsInvalidRange = true
+            }
+
+            ranges[partCategory] = newRange
+        }
+    }
+
+    return if (!containsInvalidRange) {
+        ranges
+    } else {
+        emptyMap()
+    }
+}
 
 fun parseParts(rows: List<String>) =
     rows.dropWhile { it.isNotBlank() }
         .drop(1)
         .map(::parsePart)
-
-fun parseWorkflow(workflow: String): Pair<String, List<Rule>> {
-    val (name, rest) = workflow.split("{")
-    val rules = rest.dropLast(1).split(",").map{ rule -> parseRule(rule) }
-    return name to rules
-}
-
-fun parseRule(rule: String): Rule {
-    val destinationSeparatorIndex = rule.indexOf(":")
-    return if (destinationSeparatorIndex != -1) {
-        Rule(
-            rule[0],
-            Comparator.parse(rule[1]),
-            rule.substring(2, destinationSeparatorIndex).toInt(),
-            rule.substring(destinationSeparatorIndex + 1)
-        )
-    } else {
-        Rule(null, null, null, rule)
-    }
-}
 
 fun parsePart(part: String): Part {
     val categories = part
@@ -116,13 +198,3 @@ fun parsePart(part: String): Part {
         }
     return Part(categories)
 }
-
-fun Rule.compareTo(partValue: Int): Boolean {
-    return when (this.comparator) {
-        Comparator.LESS_THAN -> partValue < this.comparatorValue!!
-        Comparator.GREATER_THAN -> partValue > this.comparatorValue!!
-        else -> throw IllegalArgumentException("Unknown comparator: $comparator")
-    }
-}
-fun Rule.accepts() = this.ifTrue == "A"
-fun Rule.rejects() = this.ifTrue == "R"
